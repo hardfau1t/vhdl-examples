4391e128af5adeda6399a7b4552496c374d64f5e
hardfau1t <hardfau1t@proton.me>
2025-07-09 15:33:38 +0530

    format hello, add gitignore

--- hello.vhd
+++ hello.vhd
@@ -1,12 +1,12 @@
 use std.textio.all;
 
 entity hello_world is
-    end hello_world;
-    
+end hello_world;
+
 architecture sim of hello_world is
 begin
-process begin
+  process begin
     write (output, "Hello world" & LF);
     wait;
-end process;
+  end process;
 end sim;
